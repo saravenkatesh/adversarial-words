eddfef33c4369649d3e86a4a5fe55221e5113f1f
Sara Venkatesh <sarajvenkatesh@gmail.com>
2022-12-21 16:12:47 -0800

    Add informative line

--- src/public/main.js
+++ src/public/main.js
@@ -7,6 +7,17 @@ const guessList = document.getElementById('guessList');
 const wordListLength = CORNCOB_WORD_LIST.length;
 const wordIndex = Math.floor((Math.random()) * wordListLength);
 const guesses = [];
+const guessHints = [];
+
+const makeGuessLine = () => {
+  const guessLine = document.createElement('hr');
+  guessLine.style.color = "red";
+  guessLine.style.backgroundColor = "red";
+  guessLine.style.borderColor = "red";
+  guessLine.style.width = '64px';
+  guessLine.style.height = "2px";
+  guessList.append(guessLine);
+}
 
 const onSubmit = () => {
   const guessWord = input.value;
@@ -18,19 +29,32 @@ const onSubmit = () => {
   };
   if (guessWord < CORNCOB_WORD_LIST[wordIndex]) {
     console.log( 'Word comes after');
+    guessHints.push('a');
   };
   if (guessWord > CORNCOB_WORD_LIST[wordIndex]) {
     console.log( 'Word comes before');
+    guessHints.push('b');
   };
   guesses.push(guessWord);
   guesses.sort();
+  guessHints.sort();
 
   guessList.innerHTML = "";
-  for (const guess of guesses) {
+  guesses.forEach((guess, index) => {
     const guessDiv = document.createElement('div');
     const text = document.createTextNode(guess);
     guessDiv.append(text);
+    if (
+        (index === 0 || guessHints[index - 1] === 'a')
+        && guessHints[index] === 'b'
+    ) {
+      makeGuessLine();
+    }
     guessList.append(guessDiv);
+  })
+
+  if (guessHints[guessHints.length - 1] === 'a') {
+    makeGuessLine();
   }
 };
 
